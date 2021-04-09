6cc6d9b6585c566aad2e708a0c4e0d8f8866612c
nanitight <71830763+nanitight@users.noreply.github.com>
2021-04-09 08:12:40 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,64 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
+import Compiler from './Compiler' ;
 import reportWebVitals from './reportWebVitals';
-// import { ReactComponent } from '*.svg';
-// import Addition from './Addition' ;
 
-// import Todo from './todo'
-// import FlashCards from './FlashCards' ; 
-
-// import Clock from './Clock' ;
-// import Todo from './todo' ;
-// import Exchange from './exchange' ;
-import Compiler from './compiler' ;
-
-// class Count extends React.Component {
-
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       num:0
-//     } ;  
-//   }
-
-
-// componentDidMount(){
-//   this.setState({num:42}) ;
-// }
-
-// componentDidUpdate(){
-//   alert("Update!") ;
-// }
-
-//   inc = () => {
-//     this.setState(oldState => ({
-//       num:oldState.num+1
-//     }));
-//   } 
-
-//   //avoids race condition //prevent against potential bugs
-
-//   dec = () =>{
-//     this.setState(state =>({
-//       num: state.num-1
-//     }));
-//   }
-//   render(){
-//     return <div>
-//       <p> Number is now: {this.state.num} </p>
-//       <button onClick={this.inc}>Increase The Number!</button>
-//       <button onClick={this.dec}>Decrement </button>
-//       </div> ;
-//   }
-// }
-
-
- // eslint-disable-next-line
-// const elem = <Converter />;
-// const count = <Count /> ;
-ReactDOM.render(  
-  <Compiler /> ,
+ReactDOM.render(
+  <React.StrictMode>
+    <Compiler />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
